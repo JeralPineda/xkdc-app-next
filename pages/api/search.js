3067981af4ce8fe4bcf3d20f04205310e7a541dc
Jeral Pineda <jeral.cris@gmail.com>
2022-10-07 11:36:00 -0600

    Agragando endpoint en api para search

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,5 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { search } from 'services/search';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const {
+    query: { q },
+  } = req;
+
+  const results = await search({ query: q });
+
+  return res.status(200).json(results);
 }
