819a057d63515fba9671a0b429d3fb618affd220
Jeral Pineda <jeral.cris@gmail.com>
2022-10-06 20:59:32 -0600

    Agregando el componente footer a la app

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer className="text-center pb-4 pt-4 font-bold">
+      <a
+        //
+        href="https://xkcd.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        All comics by xkcd
+      </a>
+    </footer>
+  );
+};
